09544c0c74ad20eb1a06624defece77bd43087c0
Sudhanshu Kulshrestha <48475411+SudKul@users.noreply.github.com>
2025-04-09 19:16:19 +0530

    Update main.tf. Change from serverless v2 to d3.t3.medium

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -23,7 +23,7 @@ resource "aws_rds_cluster" "aurora_serverless" {
 
 resource "aws_rds_cluster_instance" "aurora_instance" {
   cluster_identifier = aws_rds_cluster.aurora_serverless.id
-  instance_class     = "db.serverless"
+  instance_class     = "db.t3.medium"
   engine             = aws_rds_cluster.aurora_serverless.engine
   engine_version     = aws_rds_cluster.aurora_serverless.engine_version
 }
